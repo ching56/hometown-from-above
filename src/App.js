f6661d0f54141ebb38a248820d3a328c7ebc7b8d
Ching <dino8556@gmail.com>
2018-06-03 14:12:52 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <img src="http://via.placeholder.com/50x50" alt="logo"/>
+        看見家鄉
+      </div>
+    );
+  }
+}
+
+export default App;
